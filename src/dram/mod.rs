5661bce46115ba6b380373ea49ab8805d646e800
DrEden33773 <edwardwang33773@gmail.com>
2023-07-07 16:48:45 +0800

    better function

--- src/dram/mod.rs
+++ src/dram/mod.rs
@@ -11,81 +11,36 @@ impl Dram {
         Self { dram }
     }
     pub fn load(&self, addr: u64, size: u64) -> Result<u64, ()> {
-        match size {
-            8 => Ok(self.load8(addr)),
-            16 => Ok(self.load16(addr)),
-            32 => Ok(self.load32(addr)),
-            64 => Ok(self.load64(addr)),
-            _ => Err(()),
+        if ![8, 16, 32, 64].contains(&size) {
+            return Err(());
         }
+        Dram::load8n(self, addr, size as usize / 8)
     }
     pub fn store(&mut self, addr: u64, size: u64, value: u64) -> Result<(), ()> {
-        match size {
-            8 => {
-                self.store8(addr, value);
-                Ok(())
-            }
-            16 => {
-                self.store16(addr, value);
-                Ok(())
-            }
-            32 => {
-                self.store32(addr, value);
-                Ok(())
-            }
-            64 => {
-                self.store64(addr, value);
-                Ok(())
-            }
-            _ => Err(()),
+        if ![8, 16, 32, 64].contains(&size) {
+            return Err(());
         }
+        Dram::store8n(self, addr, value, size as usize / 8)
     }
-
-    fn load8n(&self, addr: u64, n: usize) -> u64 {
+    fn load8n(&self, addr: u64, n: usize) -> Result<u64, ()> {
+        if ![1, 2, 3, 4].contains(&n) {
+            return Err(());
+        }
         let index = (addr - DRAM_BASE) as usize;
         let mut value = 0;
         for i in 0..n {
             value |= (self.dram[index + i] as u64) << (8 * i);
         }
-        value
+        Ok(value)
     }
-    #[inline]
-    fn load8(&self, addr: u64) -> u64 {
-        Dram::load8n(self, addr, 1)
-    }
-    #[inline]
-    fn load16(&self, addr: u64) -> u64 {
-        Dram::load8n(self, addr, 2)
-    }
-    #[inline]
-    fn load32(&self, addr: u64) -> u64 {
-        Dram::load8n(self, addr, 4)
-    }
-    #[inline]
-    fn load64(&self, addr: u64) -> u64 {
-        Dram::load8n(self, addr, 8)
-    }
-
-    fn store8n(&mut self, addr: u64, value: u64, n: usize) {
+    fn store8n(&mut self, addr: u64, value: u64, n: usize) -> Result<(), ()> {
+        if ![1, 2, 3, 4].contains(&n) {
+            return Err(());
+        }
         let index = (addr - DRAM_BASE) as usize;
         for i in 0..n {
             self.dram[index + i] = ((value >> (8 * i)) & 0xff) as u8;
         }
-    }
-    #[inline]
-    fn store8(&mut self, addr: u64, value: u64) {
-        Dram::store8n(self, addr, value, 1)
-    }
-    #[inline]
-    fn store16(&mut self, addr: u64, value: u64) {
-        Dram::store8n(self, addr, value, 2)
-    }
-    #[inline]
-    fn store32(&mut self, addr: u64, value: u64) {
-        Dram::store8n(self, addr, value, 4)
-    }
-    #[inline]
-    fn store64(&mut self, addr: u64, value: u64) {
-        Dram::store8n(self, addr, value, 8)
+        Ok(())
     }
 }
